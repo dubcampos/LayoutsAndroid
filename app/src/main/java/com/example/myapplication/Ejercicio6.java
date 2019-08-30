8e16cc5dd346ecb6f93812b3285bbccdb234ef48
dubcampos <53241023+dubcampos@users.noreply.github.com>
2019-08-29 22:00:57 -0500

    avance

--- app/src/main/java/com/example/myapplication/Ejercicio6.java
+++ app/src/main/java/com/example/myapplication/Ejercicio6.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,6 +17,8 @@ public class Ejercicio6 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio6);
         display = findViewById(R.id.txt_resultado);
+        AsignarBotones();
+        AsignarEventos();
 
     }
 
@@ -24,5 +27,24 @@ public class Ejercicio6 extends AppCompatActivity {
         botones_numeros[1] = findViewById(R.id.btn_1);
         botones_numeros[2] = findViewById(R.id.btn_2);
         botones_numeros[3] = findViewById(R.id.btn_3);
+        botones_numeros[4] = findViewById(R.id.btn_4);
+        botones_numeros[5] = findViewById(R.id.btn_5);
+        botones_numeros[6] = findViewById(R.id.btn_6);
+        botones_numeros[7] = findViewById(R.id.btn_7);
+        botones_numeros[8] = findViewById(R.id.btn_8);
+        botones_numeros[9] = findViewById(R.id.btn_9);
+    }
+
+    void AsignarEventos(){
+        for(int i = 0; i<10; i++){
+            final int finalI = i;
+            botones_numeros[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    str_display += finalI;
+                    display.setText(str_display);
+                }
+            });
+        }
     }
 }
